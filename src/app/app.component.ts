14d9b06814b89c3e01a4cd55fb76343b2c3cbbf8
enayet329 <mdsakayethossen2@gmail.com>
2024-01-24 18:43:01 +0600

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Login } from './models/login';
+import { Register } from './models/register';
+import { JwtAuth } from './models/jwtAuth';
+import { AuthenticationService } from './services/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Drivers.App';
+  loginDto = new Login();
+  registerDto = new Register();
+  jwtDto = new JwtAuth();
+
+  constructor(private authService: AuthenticationService) {}
+
+  register(Register: Register) {
+    this.authService.register(Register).subscribe();
+  }
+
+  login(loginDto: Login) {
+    this.authService.login(loginDto).subscribe((jwtDto) => {
+      localStorage.setItem('jwtToken',this.jwtDto.token);
+    });
+  }
+
+  Weather(){
+    this.authService.getWeather().subscribe((weatherdata: any) => {
+      console.log(weatherdata);
+    })
+  }
 }
